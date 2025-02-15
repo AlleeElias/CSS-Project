d20249c3679d42dca5a68376909d77791e3a8934
AlleeElias <eliasneel2001@gmail.com>
2025-02-15 20:37:39 +0100

    Dynamische header

--- script/script.js
+++ script/script.js
@@ -1,6 +1,13 @@
 let actieTekst = "Je hebt op de knop geklikt!";
 
-let footerHTML = "<div>Copyright @Allee</div><div><a class='gitknop' target='_blank' rel='noopener noreferrer' href='https://github.com/AlleeElias/CSS-Project'>Github</a></div>";
+let footerHTML = "<div>Copyright @Allee</div>" +
+                 "<div><a class='gitknop' target='_blank' rel='noopener noreferrer' href='https://github.com/AlleeElias/CSS-Project'>Github</a></div>";
+
+let headerHTML = "<div class='headertitle'>Home</div>" + 
+                 "<div class='headerlist'>" +
+                 "<a href='../html/home.html'>Home</a>" +
+                 "<a href='../html/info.html'>Info</a>" +
+                 "</div>";
 
 function doeIets(){
     document.getElementById("actiediv").innerText = actieTekst;
@@ -8,4 +15,5 @@ function doeIets(){
 
 window.onload = function() { 
     document.getElementById("footerdiv").innerHTML = footerHTML;
+    document.getElementById("pageheader").innerHTML = headerHTML;
 }

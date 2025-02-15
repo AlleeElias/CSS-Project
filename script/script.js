ed654c00549cb62d7e39a0ecd520944e138e428b
AlleeElias <eliasneel2001@gmail.com>
2025-02-15 19:39:21 +0100

    Voorbereiding les 2

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 let actieTekst = "Je hebt op de knop geklikt!";
 
-let footerHTML = "<div>Copyright @Allee</div><div><a target='_blank' rel='noopener noreferrer' href='https://github.com/AlleeElias/CSS-Project'>Github</a></div>";
+let footerHTML = "<div>Copyright @Allee</div><div><a class='gitknop' target='_blank' rel='noopener noreferrer' href='https://github.com/AlleeElias/CSS-Project'>Github</a></div>";
 
 function doeIets(){
     document.getElementById("actiediv").innerText = actieTekst;

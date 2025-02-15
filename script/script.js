3918e796e60399ab2a2aba55a6a0ad9cbc68a5bb
AlleeElias <eliasneel2001@gmail.com>
2025-02-15 18:26:23 +0100

    opkuis

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let actieTekst = "Je hebt op de knop geklikt!";
+
+let footerHTML = "<div>Copyright @Allee</div><div><a href='https://github.com/AlleeElias/'>Github</a></div>";
+
+function doeIets(){
+    document.getElementById("actiediv").innerText = actieTekst;
+}
+
+window.onload = function() { 
+    document.getElementById("footerdiv").innerHTML = footerHTML;
+}
